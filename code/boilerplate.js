a82e0a725bd2d18a0ef6e6b4fc2e05954680def6
r12a <ishida@w3.org>
2015-02-15 10:05:19 +0000

    new

--- code/boilerplate.js
+++ code/boilerplate.js
@@ -7,7 +7,7 @@ function bp_header (icon, pageType, pathToRoot) {
 		+ '</ul></div>'
 		+ '<div id="site-navigation"> <img id="bp_picture" alt=" " src="'+icon+'"/></div>'
 		+ '<div id="boilerplate"><div id="line" >&nbsp;</div></div>'
-		+ '<div id="topbar"><a href="/">rishida</a> &gt;&gt; '+pageType+'</div>'
+		+ '<div id="topbar"><a href="/">r12a</a> &gt;&gt; '+pageType+'</div>'
 		+ '<div id="sitelinks" class="noprint">'
 		+ '<a href="http://rishida.net/blog/">blog</a>&nbsp; <a href="/docs">docs</a>&nbsp; <a href="/apps">apps</a>&nbsp; <a href="/photos">photos</a>&nbsp;&nbsp;</span></div>';
 	}
@@ -16,7 +16,7 @@ function bp_header (icon, pageType, pathToRoot) {
 function bp_compactHeader (icon, pageType, pathToRoot) {
 	return '<div id="site-navigation"> <img id="bp_picture" class="compact" alt=" " src="'+icon+'"  /></div>'
 		+ '<div id="boilerplate"><div id="line" class="midlength">&nbsp;</div></div>'
-		+ '<div id="topbar"><a href="/">rishida</a> &gt;&gt; '+pageType+'</div>'
+		+ '<div id="topbar"><a href="/">r12a</a> &gt;&gt; '+pageType+'</div>'
 		+ '<div id="sitelinks" class="noprint">'
 		+ '<a href="http://rishida.net/blog/">blog</a>&nbsp; <a href="/docs">docs</a>&nbsp; <a href="/apps">apps</a>&nbsp; <a href="/photos">photos</a>&nbsp;&nbsp;</span></div>'
 	}
