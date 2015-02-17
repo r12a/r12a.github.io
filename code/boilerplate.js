cd4745fa3583057514f4b9e5d3af8a532319852a
r12a <ishida@w3.org>
2015-02-17 11:44:59 +0000

    link update

--- code/boilerplate.js
+++ code/boilerplate.js
@@ -9,7 +9,7 @@ function bp_header (icon, pageType, pathToRoot) {
 		+ '<div id="boilerplate"><div id="line" >&nbsp;</div></div>'
 		+ '<div id="topbar"><a href="/">r12a</a> &gt;&gt; '+pageType+'</div>'
 		+ '<div id="sitelinks" class="noprint">'
-		+ '<a href="http://rishida.net/blog/">blog</a>&nbsp; <a href="/docs">docs</a>&nbsp; <a href="/apps">apps</a>&nbsp; <a href="/photos">photos</a>&nbsp;&nbsp;</span></div>';
+		+ '<a href="http://rishida.net/blog/">blog</a>&nbsp; <a href="/doclist">docs</a>&nbsp; <a href="/applist">apps</a>&nbsp; <a href="/photos">photos</a>&nbsp;&nbsp;</span></div>';
 	}
 
 
@@ -18,5 +18,5 @@ function bp_compactHeader (icon, pageType, pathToRoot) {
 		+ '<div id="boilerplate"><div id="line" class="midlength">&nbsp;</div></div>'
 		+ '<div id="topbar"><a href="/">r12a</a> &gt;&gt; '+pageType+'</div>'
 		+ '<div id="sitelinks" class="noprint">'
-		+ '<a href="http://rishida.net/blog/">blog</a>&nbsp; <a href="/docs">docs</a>&nbsp; <a href="/apps">apps</a>&nbsp; <a href="/photos">photos</a>&nbsp;&nbsp;</span></div>'
+		+ '<a href="http://rishida.net/blog/">blog</a>&nbsp; <a href="/doclist">docs</a>&nbsp; <a href="/applist">apps</a>&nbsp; <a href="/photos">photos</a>&nbsp;&nbsp;</span></div>'
 	}
