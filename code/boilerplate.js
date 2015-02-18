cb399cc2db0da87309637e1c15a8216e83679427
r12a <ishida@w3.org>
2015-02-18 11:43:31 +0000

    added appHeader, toggleNotes functions

--- code/boilerplate.js
+++ code/boilerplate.js
@@ -20,3 +20,26 @@ function bp_compactHeader (icon, pageType, pathToRoot) {
 		+ '<div id="sitelinks" class="noprint">'
 		+ '<a href="http://rishida.net/blog/">blog</a>&nbsp; <a href="/doclist">docs</a>&nbsp; <a href="/applist">apps</a>&nbsp; <a href="/photos">photos</a>&nbsp;&nbsp;</span></div>'
 	}
+
+
+function bp_appHeader (icon, appname, version) {
+	return '<div id="site-navigation"> <img id="bp_picture" class="app" alt=" " src="'+icon+'"  /></div>'
+		+ '<div id="boilerplate"><div id="line" class="app">&nbsp;</div></div>'
+		+ '<div id="topbar"><a href="/">r12a</a> &gt;&gt; apps &gt;&gt; '+appname+' <span class="versionnumber">'+version+'</span></div>'
+		+ '<div id="sitelinks" class="noprint">'
+		+ '<a href="http://rishida.net/blog/">blog</a>&nbsp; <a href="/doclist">docs</a>&nbsp; <a href="/applist">apps</a>&nbsp; <a href="/photos">photos</a>&nbsp;&nbsp;</span></div>'
+	}
+
+
+function toggleNotes () {
+	var notes = document.getElementById('detailednotes')
+	var showNotes = document.getElementById('showNotes')
+	if (notes.style.display=='block') {
+		notes.style.display='none' 
+		showNotes.textContent='show notes'
+		} 
+	else {
+		notes.style.display='block'
+		showNotes.textContent='hide notes'
+		} 
+	}
