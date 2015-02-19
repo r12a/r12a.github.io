2383be59f29a9cb459cb4805a058d52d4294ec96
r12a <ishida@w3.org>
2015-02-19 12:16:06 +0000

    new

---
+++ pickers/uighur/defaults.js
@@ -0,0 +1,13 @@
+var defaults = new Object;
+
+defaults.font = '"Microsoft Uighur", "Scheherazade", "Geeza Pro", "Arabic Typesetting", "Traditional Arabic", "Noto Naskh Arabic", sans-serif'; // font name per usage in CSS
+defaults.size = "50";  // number of pixels (just the number)
+defaults.rows = "2"; // number representing number of times 100px for height of box
+defaults.lineHeight = "1.8";  // just the number
+defaults.view = "alphabet";  // alphabet, phonic, phonelist
+defaults.language = 'ug'
+defaults.blocklocation= '/scripts/block/arabic'
+defaults.uifont = '"Microsoft Uighur", "Scheherazade", "Geeza Pro", "Arabic Typesetting", "Traditional Arabic", "Noto Naskh Arabic", sans-serif'; // font name per usage in CSS
+defaults.uisize = "40";  // number of pixels (just the number)
+
+
