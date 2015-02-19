6332b9d6e9968709daf22db2c4105c06c6af8d45
r12a <ishida@w3.org>
2015-02-19 08:17:33 +0000

    new

---
+++ pickers/gurmukhi/defaults.js
@@ -0,0 +1,13 @@
+var defaults = new Object;
+
+defaults.font = 'Raavi, "Nirmala UI", "Gurmukhi MT", "Gurmukhi MN", "Arial Unicode MS", sans-serif'; // font name per usage in CSS
+defaults.size = "50";  // number of pixels (just the number)
+defaults.rows = "2"; // number representing number of times 100px for height of box
+defaults.lineHeight = "1.8";  // just the number
+defaults.view = "alphabet";  // alphabet, phonic, phonelist
+defaults.language = 'pa'
+defaults.blocklocation= '/rishida/scripts/block/gurmukhi'
+defaults.uifont = '"Noto Sans Gurmukhi", "Gurmukhi MN", Raavi, "Nirmala UI", "Arial Unicode MS", sans-serif'; // font name per usage in CSS
+defaults.uisize = "28";  // number of pixels (just the number)
+
+
