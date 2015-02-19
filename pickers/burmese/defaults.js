078fbe2257fb2bc017603a4886de384be5bece93
r12a <ishida@w3.org>
2015-02-19 10:33:22 +0000

    font names, ui font selector

--- pickers/burmese/defaults.js
+++ pickers/burmese/defaults.js
@@ -1,12 +1,12 @@
 var defaults = new Object;
 
-defaults.font = 'Myanmar3Webfont' // font name per usage in CSS
+defaults.font = 'Myanmar3 WF' // font name per usage in CSS
 defaults.size = "32";  // number of pixels (just the number)
 defaults.rows = "1.5"; // number representing number of times 100px for height of box
 defaults.view = "alphabet";  // alphabet, fontgrid
 defaults.language = 'my'
 defaults.blocklocation= '/scripts/block/myanmar'
-defaults.uifont = 'Myanmar3Webfont'; // font name per usage in CSS
+defaults.uifont = 'Myanmar3 WF'; // font name per usage in CSS
 defaults.uisize = "28";  // number of pixels (just the number)
 
 
