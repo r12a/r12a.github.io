aff3c51cb085fa87e56d663d416ae405e861918b
r12a <ishida@w3.org>
2015-02-20 21:44:33 +0000

    new

---
+++ pickers/ethiopic/defaults.js
@@ -0,0 +1,14 @@
+var defaults = new Object;
+
+defaults.font = '"Noto Sans Ethiopic WF"'; // font name per usage in CSS
+defaults.size = "45";  // number of pixels (just the number)
+defaults.rows = "2"; // number representing number of times 100px for height of box
+defaults.lineHeight = "1.5";  // just the number
+defaults.view = "alphabet";  // alphabet, phonic, phonic2, phonelist
+defaults.language = 'am'
+defaults.blocklocation= '/scripts/block/ethiopic'
+defaults.uifont = 'Noto Sans Ethiopic WF'; // font name per usage in CSS
+defaults.uisize = "32";  // number of pixels (just the number)
+defaults.ccbase = "";  // default base for combining characters
+
+
