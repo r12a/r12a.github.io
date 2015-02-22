951fa5b69fb432c91638bc5c69a6d6e1a70b59d9
r12a <ishida@w3.org>
2015-02-22 18:44:28 +0000

    new

---
+++ scripts/tibetan/fonts.js
@@ -0,0 +1,30 @@
+ var fontSelection = '<optgroup label="Webfont">\
+        <option value="Noto Serif Thai">Tibetan Machine Uni WF</option>\
+        </optgroup>\
+        \
+        <option value="0">Select...</option>\
+		<optgroup label="Mac fonts ">\
+		<option value="Kailasa">Kailasa</option>\
+		<option value="Kokonor">Kokonor</option>\
+        </optgroup>\
+        \
+        <optgroup label="Windows fonts ">\
+        <option value="Microsoft Himalaya">Microsoft Himalaya</option>\
+        </optgroup>\
+        \
+		<optgroup label="Other fonts ">\
+		<option value="Jomolhari">Jomolhari</option>\
+        <option value="Tibetan Machine Uni">Tibetan Machine Uni</option>\
+        <option value="Uchen_05">Uchen_05</option>\
+        <option value="Wangdi29">Wangdi29</option>\
+        <option value="XenoType Tibetan New">XenoType Tibetan New</option>\
+        <option value="XTashi">XTashi</option>\
+        </optgroup>\
+        \
+        <optgroup label="General fonts ">\
+        <option value="Arial Unicode MS">Arial Unicode MS</option>\
+        <option value="Code2000">Code2000</option>\
+        <option value="SimSun-18030">SimSun-18030</option>\
+        <option value="sans-serif">sans-serif</option>\
+        <option value="serif">serif</option>\
+        </optgroup>'
