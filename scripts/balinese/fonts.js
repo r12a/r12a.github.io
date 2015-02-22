6ccce4f5f3bef58792d63be3146100a30f4f56a0
r12a <ishida@w3.org>
2015-02-22 15:24:01 +0000

    new

---
+++ scripts/balinese/fonts.js
@@ -0,0 +1,12 @@
+ var fontSelection = '     <option value="0">Select...</option>\
+             <optgroup label="Web font">\
+            <option value="Noto Sans Balinese WF">Noto Sans Balinese WF</option>\
+            </optgroup>\
+            \
+            <optgroup label="Google fonts ">\
+            <option value="Noto Sans Balinese">Noto Sans Balinese</option>\
+            </optgroup>\
+            \
+            <optgroup label="Other fonts ">\
+            <option value="Aksara Bali">Aksara Bali</option>\
+            </optgroup>'
