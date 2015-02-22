7167a230c1735fdebb7161c8a3f1fbd87f618d43
r12a <ishida@w3.org>
2015-02-22 15:22:51 +0000

    new

---
+++ scripts/arabic/fonts.js
@@ -0,0 +1,73 @@
+ var fontSelection = '<option value="0">Select...</option>\
+            <optgroup label="Web fonts ">\
+            <option value="Scheherazade WF">Scheherazade WF</option>\
+            <option value="Nafees Nastaliq WF">Nafees Nastaliq WF</option>\
+            </optgroup>\
+            \
+          <option value="0">Select...</option>\
+            <optgroup label="Mac fonts ">\
+            <option value="Al Bayan">Al Bayan</option>\
+            <option value="Baghdad">Baghdad</option>\
+            <option value="Decotype Naskh">Decotype Naskh</option>\
+            <option value="Geeza Pro">Geeza Pro</option>\
+            <option value="KufiStandardGK">KufiStandardGK</option>\
+            <option value="Nadeem">Nadeem</option>\
+            <option value="Damascus">Damascus</option>\
+            </optgroup>\
+            \
+            <optgroup label="Windows fonts ">\
+            <option value="Arabic Typesetting">Arabic Typesetting</option>\
+            <option value="Aldhabi">Aldhabi</option>\
+            <option value="Andalus">Andalus</option>\
+            <option value="Microsoft Uighur">Microsoft Uighur </option>\
+            <option value="Sakkal Majalla">Sakkal Majalla</option>\
+            <option value="Simplified Arabic">Simplified Arabic</option>\
+            <option value="Simplified Arabic Fixed">Simplified Arabic Fixed</option>\
+            <option value="Traditional Arabic">Traditional Arabic</option>\
+            <option value="Urdu Typesetting">Urdu Typesetting</option>\
+            </optgroup>\
+            \
+            <optgroup label="Google fonts ">\
+            <option value="Scheherazade">Scheherazade</option>\
+            <option value="Noto Naskh Arabic">Noto Naskh Arabic</option>\
+            </optgroup>\
+            \
+            <optgroup label="Other fonts ">\
+            <option value="Arabic Transparent">Arabic Transparent</option>\
+            <option value="Farsi Simple">Farsi Simple</option>\
+            <option value="Koodak">Koodak</option>\
+            <option value="Koodak">Koodak</option>\
+            <option value="IranNastaliq">IranNastaliq</option>\
+            <option value="mry_KacstQurn">mry_KacstQurn</option>\
+            <option value="Nafees Naskh">Nafees Naskh</option>\
+            <option value="Nafees Pakistani Naskh">Nafees Pakistani Naskh</option>\
+            <option value="Nafees Nastaleeq">Nafees Nastaleeq</option>\
+            <option value="Nafees Web Naskh">Nafees Web Naskh</option>\
+            <option value="Nastaleeq Like">Nastaleeq Like</option>\
+            <option value="Nazli">Nazli</option>\
+            <option value="PakType Tehreer">PakType Tehreer</option>\
+            <option value="Pashtu Abdaali">Pashtu Abdaali</option>\
+            <option value="Pashtu Waziristan">Pashtu Waziristan</option>\
+            <option value="Riwaj">Riwaj</option>\
+            <option value="Roya">Roya</option>\
+            <option value="Terafik">Terafik</option>\
+            <option value="Unikurd Tishk">Unikurd Tishk</option>\
+            <option value="Unikurd Web">Unikurd Web</option>\
+            <option value="Urdu Naskh Unicode">Urdu Naskh Unicode</option>\
+            <option value="Urdu Nastaliq Unicode">Urdu Nastaliq Unicode</option>\
+            </optgroup>\
+            \
+            <optgroup label="General fonts ">\
+            <option value="Arial">Arial</option>\
+            <option value="Arial Unicode MS">Arial Unicode MS</option>\
+            <option value="Bitstream Cyberbase">Bitstream Cyberbase</option>\
+            <option value="Bitstream Cyberbit">Bitstream Cyberbit</option>\
+            <option value="Code2000">Code2000</option>\
+            <option value="Courier New">Courier New</option>\
+            <option value="DejaVu Sans">DejaVu Sans</option>\
+            <option value="Tahoma">Tahoma</option>\
+            <option value="Times New Roman">Times New Roman</option>\
+            <option value="sans-serif">sans-serif</option>\
+            <option value="serif">serif</option>\
+            </optgroup>\
+        '
