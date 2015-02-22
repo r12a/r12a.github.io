f43491f8c70a04c94903bf6235aea7c0b108a0b5
r12a <ishida@w3.org>
2015-02-22 17:35:34 +0000

    new

---
+++ scripts/myanmar/fonts.js
@@ -0,0 +1,23 @@
+ var fontSelection = '<optgroup label="Webfonts">\
+        <option value="Myanmar3 WF">Myanmar3</option>\
+        <option value="Tharlon WF">Tharlon</option>\
+        <option value="Noto Sans Myanmar WF">Noto Sans Myanmar</option>\
+        </optgroup>\
+        <optgroup label="Mac fonts">\
+        <option value="Myanmar MN">Myanmar MN</option>\
+        <option value="Myanmar Sangam MN">Myanmar Sangam MN</option>\
+        </optgroup>\
+        \
+        <optgroup label="Windows fonts">\
+        <option value="Myanmar Text">Myanmar Text</option>\
+        </optgroup>\
+        \
+        <optgroup label="Google fonts">\
+        <option value="Tharlon">Tharlon</option>\
+        <option value="Noto Sans Myanmar">Noto Sans Myanmar</option>\
+        <option value="Padauk">Padauk</option>\
+        </optgroup>\
+        \
+        <optgroup label="Other fonts">\
+        <option value="Myanmar3">Myanmar3</option>\
+        </optgroup>'
