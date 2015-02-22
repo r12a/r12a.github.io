97ff0d29aca5be3b46d3bd4ede98b2866edf45a8
r12a <ishida@w3.org>
2015-02-22 17:15:35 +0000

    new

---
+++ scripts/mongolian/fonts.js
@@ -0,0 +1,7 @@
+ var fontSelection = '<option value="Mongolian White">Mongolian White</option>\
+<option value="Mongolian Writing">Mongolian Writing</option>\
+<option value="Mongolian Baiti">Mongolian Baiti</option>\
+<option value="Noto Sans Mongolian">Noto Sans Mongolian</option>\
+<option value="Noto Sans Mongolian WF">Noto Sans Mongolian WF</option>\
+							<option value="sans-serif">sans-serif</option>\
+							<option value="serif">serif</option>'
