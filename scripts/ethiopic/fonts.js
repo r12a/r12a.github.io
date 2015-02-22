5aa227e9f17c6d57136daef1362726a9dc99b1f1
r12a <ishida@w3.org>
2015-02-22 15:25:44 +0000

    new

---
+++ scripts/ethiopic/fonts.js
@@ -0,0 +1,43 @@
+ var fontSelection = '<optgroup label="Web font">\
+      <option value="Noto Sans Ethiopic WF">Noto Sans Ethiopic WF</option>\
+      </optgroup>\
+        <optgroup label="Mac fonts ">\
+        <option value="Kefo">Kefa</option>\
+        </optgroup>\
+        \
+        <optgroup label="Windows fonts ">\
+        <option value="Nyala">Nyala</option>\
+        </optgroup>\
+        \
+        <optgroup label="Google fonts ">\
+        <option value="Droid Sans Ethiopic">Droid Sans Ethiopic</option>\
+        </optgroup>\
+        \
+        <optgroup label="Other fonts ">\
+        <option value="Abyssinica SIL">Abyssinica SIL</option>\
+        <option value="AmharicLSU">AmharicLSU</option>\
+        <option value="Ethiopia Jiret">Ethiopia Jiret</option>\
+        <option value="Ethiopia Jiret Slant">Ethiopia Jiret Slant</option>\
+        <option value="Ethiopic Fantuwua">Ethiopic Fantuwua</option>\
+        <option value="Ethiopic Hiwua">Ethiopic Hiwua</option>\
+        <option value="Ethiopic Tint">Ethiopic Tint</option>\
+        <option value="Ethiopic Wookianos">Ethiopic Wookianos</option>\
+        <option value="Ethiopic Yebse">Ethiopic Yebse </option>\
+        <option value="Ethiopic Yigezu Bisrat Goffer">Ethiopic Yigezu Bisrat Goffer</option>\
+        <option value="EthiopicLSU">EthiopicLSU</option>\
+        <option value="Geez Unicode">Geez Unicode</option>\
+        <option value="GF Zemen Unicode">GF Zemen Unicode</option>\
+        <option value="GS GeezMahtemUnicode">GS GeezMahtemUnicode</option>\
+        <option value="Visual Geez Unicode">Visual Geez Unicode</option>\
+        <option value="Visual Geez Unicode Agazian">Visual Geez Unicode Agazian</option>\
+        <option value="Visual Geez Unicode Title">Visual Geez Unicode Title</option>\
+        </optgroup>\
+        \
+        <optgroup label="General fonts ">\
+        <option value="Arial Unicode MS">Arial Unicode MS</option>\
+        <option value="Code2000">Code2000</option>\
+        <option value="Free Serif">Free Serif</option>\
+        <option value="sans-serif">sans-serif</option>\
+        <option value="serif">serif</option>\
+        </optgroup>\
+'
