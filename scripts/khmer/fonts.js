aa3ab9424b41f21a201a391214945fa56152f83f
r12a <ishida@w3.org>
2015-02-22 15:26:33 +0000

    new

---
+++ scripts/khmer/fonts.js
@@ -0,0 +1,42 @@
+ var fontSelection = '<optgroup label="Webfonts">\
+      <option value="Battambang WF">Battambang WF</option>\
+      <option value="Noto Sans Khmer WF">Noto Sans Khmer WF</option>\
+      </optgroup>\
+      <optgroup label="Mac fonts">\
+      <option value="Khmer MN">Khmer MN (Lion)</option>\
+      <option value="Khmer MN">Khmer Sangam MN (Lion)</option>\
+      </optgroup>\
+      <optgroup label="Windows fonts">\
+      <option value="DaunPenh">DaunPenh (Windows7)</option>\
+      <option value="MoolBoran">MoolBoran (Windows7)</option>\
+      </optgroup>\
+      <optgroup label="Google fonts">\
+      <option value="Noto Sans">Noto Sans Khmer</option>\
+      </optgroup>\
+      <optgroup label="Other fonts">\
+      <option value="Battambang">Battambang</option>\
+      <option value="CDT Khmer">CDT Khmer</option>\
+      <option value="Khmer Angkulileka">Khmer Angkulileka</option>\
+      <option value="Khmer Chanta">Khmer Chanta</option>\
+      <option value="Khmer Kampongtrach">Khmer Kampongtrach</option>\
+      <option value="Khmer Kampot">Khmer Kampot</option>\
+      <option value="Khmer Kep">Khmer Kep</option>\
+      <option value="Khmer Kolab">Khmer Kolab</option>\
+      <option value="Khmer Mondulkiri-s">Khmer Mondulkiri-s</option>\
+      <option value="Khmer Mool">Khmer Mool</option>\
+      <option value="Khmer Nettra">Khmer Nettra</option>\
+      <option value="Khmer Old Style">Khmer Old Style</option>\
+      <option value="Khmer OS">Khmer OS</option>\
+      <option value="Khmer OS Battambang">Khmer OS Battambang</option>\
+      <option value="Khmer OS Fasthand">Khmer OS Fasthand</option>\
+      <option value="Khmer OS Freehand">Khmer OS Freehand</option>\
+      <option value="Khmer OS System">Khmer OS System</option>\
+      <option value="Khmer Vanara">Khmer Vanara</option>\
+      <option value="PhnomPenh OT">PhnomPenh OT</option>\
+      </optgroup>\
+      <optgroup label="General fonts">\
+      <option value="Code2000">Code2000</option>\
+      <option value="Arial Unicode MS">Arial Unicode MS</option>\
+      <option value="sans-serif">sans-serif</option>\
+      <option value="serif">serif</option>\
+      </optgroup>'
