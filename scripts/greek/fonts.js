042e785fff110279126a89e48c0933a454378ede
r12a <ishida@w3.org>
2015-02-22 15:26:01 +0000

    new

---
+++ scripts/greek/fonts.js
@@ -0,0 +1,5 @@
+ var fontSelection = '<option value="Noto Serif">Noto Serif</option>\
+        <option value="Gentium">Gentium</option>\
+        <option value="Doulos SIL">Doulos SIL</option>\
+        <option value="Times New Roman">Times New Roman</option>\
+'
