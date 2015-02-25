931e0367a9b1d28fddaf5958244540c24d92da73
r12a <ishida@w3.org>
2015-02-25 12:08:41 +0000

    new

---
+++ pickers/tibetan/defaults.js
@@ -0,0 +1,14 @@
+var defaults = new Object;
+
+defaults.font = "'Tibetan Machine Uni WF'"; // font name per usage in CSS
+defaults.size = "40";  // number of pixels (just the number)
+defaults.rows = "2"; // number representing number of times 100px for height of box
+defaults.lineHeight = "1.8";  // just the number
+defaults.view = "alphabet";  // alphabet, phonic, phonelist
+defaults.language = 'bo'
+defaults.blocklocation= '/scripts/tibetan/block'
+defaults.uifont = "'Tibetan Machine Uni WF'"; // font name per usage in CSS
+defaults.uisize = "32";  // number of pixels (just the number)
+defaults.ccbase = "\u0020";  // default base for combining characters
+
+
