253f1bfacbc15e63503b2a037bc7638a63b12528
r12a <ishida@w3.org>
2015-02-26 06:59:35 +0000

    new

---
+++ pickers/lisu/defaults.js
@@ -0,0 +1,13 @@
+var defaults = new Object;
+
+defaults.font = 'Noto Sans Lisu' // font name per usage in CSS
+defaults.size = "32";  // number of pixels (just the number)
+defaults.rows = "1.5"; // number representing number of times 100px for height of box
+defaults.view = "alphabet";  // alphabet, fontgrid
+defaults.language = 'my'
+defaults.blocklocation= '/scripts/myanmar/block'
+defaults.uifont = 'Noto Sans Lisu'; // font name per usage in CSS
+defaults.uisize = "24";  // number of pixels (just the number)
+defaults.ccbase = "";  // default base for combining characters
+
+
