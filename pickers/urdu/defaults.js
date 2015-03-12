3cc24467f80e6c8bc05f8a684c2dd37b6cf92e1c
r12a <ishida@w3.org>
2015-03-12 21:41:17 +0000

    new

---
+++ pickers/urdu/defaults.js
@@ -0,0 +1,14 @@
+var defaults = new Object;
+
+defaults.font = '"Noto Nastaliq Urdu WF"'; // font name per usage in CSS
+defaults.size = "30";  // number of pixels (just the number)
+defaults.rows = "2"; // number representing number of times 100px for height of box
+defaults.lineHeight = "3";  // just the number
+defaults.view = "alphabet";  // alphabet, phonic, phonelist
+defaults.language = 'ur'
+defaults.blocklocation= '/scripts/arabic/block'
+defaults.uifont = '"Noto Nastaliq Urdu WF"'; // font name per usage in CSS
+defaults.uisize = "20";  // number of pixels (just the number)
+defaults.ccbase = "◌";  // default base for combining characters
+
+
