e143c52f51a8d1627bca58ff768a57f5c7e41014
r12a <ishida@w3.org>
2015-03-13 09:34:24 +0000

    new

---
+++ pickers/tifinagh/defaults.js
@@ -0,0 +1,13 @@
+var defaults = new Object;
+
+defaults.font = '"Hapax Berbère", "Noto Sans Tifinagh WF"' // font name per usage in CSS
+defaults.size = "32";  // number of pixels (just the number)
+defaults.rows = "1.5"; // number representing number of times 100px for height of box
+defaults.view = "alphabet";  // alphabet, fontgrid
+defaults.language = 'ber'
+defaults.blocklocation= '/scripts/tifinagh/block'
+defaults.uifont = '"Hapax Berbère", "Noto Sans Tifinagh WF"'; // font name per usage in CSS
+defaults.uisize = "28";  // number of pixels (just the number)
+defaults.ccbase = "";  // default base for combining characters
+
+
