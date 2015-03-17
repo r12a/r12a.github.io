c6239b16d59a01dc694f1d879c5c48091806873e
r12a <ishida@w3.org>
2015-03-17 07:35:54 +0000

    new

---
+++ pickers/tlicho/localcode.js
@@ -0,0 +1,22 @@
+var _showMesherTrans =  ''
+var _showISOTrans = ''
+
+
+function localInitialise () {
+	document.getElementById('keyboard').style.display = 'block'
+	}
+	
+
+var _h = {
+
+end: {}
+}
+
+
+
+var keyboarddef = [
+"|1,|2,|3,|4,|5,|6,|7,|8,‹|9,›|0,«|»,‘|̀",
+"Q|q,W|w,E|e,R|r,T|t,Y|y,U|u,I|ı,O|o,P|p,“|̨,”|̨̀",
+"A|a,S|s,D|d,F|f,G|g,H|h,J|j,K|k,L|l,Ł|ł,|’",
+"Z|z,X|x,C|c,V|v,B|b,N|n,M|m,|;,.|:,Ɂ|ɂ"
+]
