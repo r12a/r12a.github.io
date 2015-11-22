5fca56cd260e676600eb6978270d046e47f99805
r12a <ishida@w3.org>
2015-11-22 07:50:44 +0000

    increased height of output box

--- pickers/mongolian/defaults.js
+++ pickers/mongolian/defaults.js
@@ -2,7 +2,7 @@ var defaults = new Object;
 
 defaults.font = 'Noto Sans Mongolian WF' // font name per usage in CSS
 defaults.size = "32";  // number of pixels (just the number)
-defaults.rows = "1.5"; // number representing number of times 100px for height of box
+defaults.rows = "4"; // number representing number of times 100px for height of box
 defaults.view = "alphabet";  // alphabet, fontgrid
 defaults.language = 'mn'
 defaults.blocklocation= '/scripts/mongolian/block'
