2b3a997e7314e0ec7f0aac5c92788e8606c83ccf
r12a <ishida@w3.org>
2015-12-06 11:05:16 +0000

    new

---
+++ pickers/arabic/defaults.js
@@ -0,0 +1,14 @@
+var defaults = new Object;
+
+defaults.font = '"Scheherazade WF"'; // font name per usage in CSS
+defaults.size = "50";  // number of pixels (just the number)
+defaults.rows = "2.5"; // number representing number of times 100px for height of box
+defaults.lineHeight = "1";  // just the number
+defaults.view = "alphabet";  // alphabet, phonic, phonelist
+defaults.language = 'ar'
+defaults.blocklocation= '/scripts/arabic/block'
+defaults.uifont = '"Scheherazade WF"'; // font name per usage in CSS
+defaults.uisize = "40";  // number of pixels (just the number)
+defaults.ccbase = "◌";  // default base for combining characters
+
+
---
+++ pickers/arabic/localcode.js
@@ -0,0 +1,67 @@
+var _showLOCTrans = ''
+var _showIPATrans = ''
+
+function localInitialise () {
+
+	}
+
+
+_characterSet = 
+"ئا ا ئە ە ب پ ت ج چ خ د ر ز ژ س ش غ ف ق ك گ ڭ ل م ن ھ ئو و ئۇ ۇ ئۆ ۆ ئۈ ۈ ۋ ئې ې ئى ىي ي ٠ ١ ٢ ٣ ٤ ٥ ٦ ٧ ٨ ٩ ؟ ، ؛"
+
+
+var _h = {
+'0627': ['06F1'],
+'0622': ['06F1'],
+'0623': ['06F1'],
+'0625': ['06F1'],
+'0671': ['06F1'],
+'06F1': ['0671', '0625', '0623', '0622', '0627'],
+'0639': ['060F'],
+'063A': ['060F'],
+'060F': ['0639', '063A'],
+'0657': ['064F'],
+'0602': ['060E'],
+'060E': ['0602'],
+'066B': ['0631'],
+'0631': ['066B'],
+
+end: {}
+}
+
+
+
+
+
+function makeCharacterLink (cp, block, lang, direction) { 
+	// returns markup with information about cp
+	// cp: a unicode character, or sequence of unicode characters
+	// block: 
+	// lang: the BCP47 language tag for the context
+	// direction: either rtl or ltr
+	var chars = cp.split('')
+
+	var out = ''
+	for (var i=0;i<chars.length;i++) {
+		var hex = convertChar2CP(chars[i])
+		var name = charData[chars[i]]['n']
+		var mark = charData[chars[i]]['m']
+		var cbase = ''
+		if (defaults.ccbase != '') cbase = '&amp;#x'+convertChar2CP(defaults.ccbase)+';'
+	
+		out +=  '<a href="/scripts/'+block+'/block#char'+hex+'"><span class="uname">U+'+hex+' '+name+'</span> (<span lang="'+lang+'"'
+		if (direction == 'rtl') { out += ' dir="rtl"' }
+		out += '>'
+		if (mark) { out += cbase }
+		out += '&#x'+hex+';</span>)</a> '
+		}
+		
+	out = '<textarea id="characterLink" style="margin-left: 20px; width: 60%; border:0;">'+out+'</textarea>'
+	
+	document.getElementById('transcriptionWrapper').innerHTML = out +
+	'<div id="closeTranscription" onclick="closeTranscription()">X</div>'
+	document.getElementById('characterLink').select();
+	}
+
+
+// charData[chars.charAt(c)].n
