6a962b9936aebd6134eca29c08a89ab108109a88
r12a <ishida@w3.org>
2015-12-21 19:51:36 +0000

    arabic picker: v18 initial changes

--- pickers/arabic/defaults.js
+++ pickers/arabic/defaults.js
@@ -2,8 +2,8 @@ var defaults = new Object;
 
 defaults.font = '"Scheherazade WF"'; // font name per usage in CSS
 defaults.size = "50";  // number of pixels (just the number)
-defaults.rows = "2.5"; // number representing number of times 100px for height of box
-defaults.lineHeight = "1.5";  // just the number
+defaults.rows = "3"; // number representing number of times 100px for height of box
+defaults.lineHeight = "2";  // just the number
 defaults.view = "alphabet";  // alphabet, phonic, phonelist
 defaults.language = 'ar'
 defaults.blocklocation= '/scripts/arabic/block'
