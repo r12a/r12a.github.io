f965667543a53d85ee9d70bcd228af22f338f5f9
r12a <ishida@w3.org>
2015-12-30 09:17:17 +0000

    new old germanic fonts file

---
+++ scripts/latin/oldgermanicfonts.js
@@ -0,0 +1,23 @@
+ var fontSelection = '<optgroup label="Webfont">\
+      <option value="Doulos SIL WF">Doulos SIL WF</option>\
+      <option value="Noto Sans Runic WF">Noto Sans Runic WF</option>\
+      </optgroup>\
+      <optgroup label="Latin fonts">\
+                <option value="Doulos SIL">Doulos SIL</option>\
+                <option value="Charis SIL">Charis SIL</option>\
+                <option value="Gentium">Gentium</option>\
+                <option value="Gentium Plus">Gentium Plus</option>\
+      </optgroup>\
+      <optgroup label="Runic fonts">\
+               <option value="Noto Sans Runic">Noto Sans Runic</option>\
+               <option value="Segoe UI Historic">Segoe UI Historic</option>\
+               <option value="Geneva">Geneva</option>\
+      </optgroup>\
+      <optgroup label="Other fonts">\
+                <option value="Lucida Sans">Lucida Sans</option>\
+                <option value="Lucida Sans Unicode">Lucida Sans Unicode</option>\
+               <option value="Arial Unicode MS">Arial Unicode MS</option>\
+                <option value="Code2000">Code2000</option>\
+                <option value="sans-serif">sans-serif</option>\
+                <option value="serif">serif</option>\
+      </optgroup>'
