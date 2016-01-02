970a1dc8c2882ef1831e68c66398f518d12eea89
r12a <ishida@w3.org>
2016-01-02 12:23:57 +0000

    publication version

--- pickers/oldenglish/localtranscription.js
+++ pickers/oldenglish/localtranscription.js
@@ -1,10 +1,7 @@
 ﻿function localtranscribe (direction, str) {
 	
 	if (direction == 'toIPA') { return oldEnglishToIPA(str) }
-	if (direction == 'toGloss') { return transcribeforGlossary(str) }
-	if (direction == 'fromNItON') { return transcribeFromNItON(str) }
-	if (direction == 'toRunes') { return oldNorseToRune(str) }
-	if (direction == 'toShortRunes') { return oldNorseToShortRune(str) }
+	if (direction == 'oldEnglishToRune') { return oldEnglishToRune(str) }
 	}
 
 
@@ -111,156 +108,76 @@ function oldEnglishToIPA (str) {
 
 
 
-function oldNorseToRune (str) {
+function oldEnglishToRune (str) {
 	// transcribes vowelled Old Norse text into a rough LB runic transcription
 	
 	
 	// add space so that beginning of word rules work
 	str = ' '+str.toLowerCase()+' '
 	str = str.replace(/[\s]+/g,' ')
-	
-	str = str.replace(/bb/g,'ᛒ') 
-	str = str.replace(/dd/g,'ᛏ') 
-	str = str.replace(/b/g,'ᛒ') 
-	str = str.replace(/d/g,'ᛏ') 
-	str = str.replace(/ð/g,'ᚦ') 
+
 	str = str.replace(/ff/g,'ᚠ') 
 	str = str.replace(/f/g,'ᚠ') 
-	str = str.replace(/gg/g,'ᚴ') 
-	str = str.replace(/g/g,'ᚴ') 
-	str = str.replace(/h/g,'ᚼ') 
-	str = str.replace(/j/g,'ᛁ') 
-	str = str.replace(/kk/g,'ᚴ') 
-	str = str.replace(/ll/g,'ᛚ') 
-	str = str.replace(/mm/g,'ᛘ') 
+	str = str.replace(/þ/g,'ᚦ') 
+	str = str.replace(/ð/g,'ᚦ') 
+	str = str.replace(/rr/g,'ᚱ') 
+	str = str.replace(/r/g,'ᚱ') 
+	str = str.replace(/cc/g,'ᚳ') 
+	str = str.replace(/c/g,'ᚳ') 
+	str = str.replace(/gg/g,'ᚷ') 
+	str = str.replace(/g/g,'ᚷ') 
+	str = str.replace(/w/g,'ᚹ') 
+	str = str.replace(/h/g,'ᚻ') 
 	str = str.replace(/nn/g,'ᚾ') 
-	str = str.replace(/k/g,'ᚴ') 
-	str = str.replace(/l/g,'ᛚ') 
-	str = str.replace(/m/g,'ᛘ') 
 	str = str.replace(/n/g,'ᚾ') 
-	str = str.replace(/pp/g,'ᛒ') 
-	str = str.replace(/rr/g,'ᚱ') 
-	str = str.replace(/ʀ/g,'ᛦ') 
+	str = str.replace(/j/g,'ᛡ') 
+	str = str.replace(/pp/g,'ᛈ') 
+	str = str.replace(/p/g,'ᛈ') 
+	str = str.replace(/x/g,'ᛉ') 
 	str = str.replace(/ss/g,'ᛋ') 
-	str = str.replace(/tt/g,'ᛏ') 
-	str = str.replace(/p/g,'ᛒ') 
-	str = str.replace(/r/g,'ᚱ') 
 	str = str.replace(/s/g,'ᛋ') 
+	str = str.replace(/tt/g,'ᛏ') 
 	str = str.replace(/t/g,'ᛏ') 
-	str = str.replace(/v/g,'ᚢ') 
-	str = str.replace(/þ/g,'ᚦ') 
-	str = str.replace(/v/g,'ᚢ') 
-	str = str.replace(/z/g,'ts') 
-	
-	str = str.replace(/a/g,'ᛅ') 
-	str = str.replace(/á/g,'ᛅ') 
-	str = str.replace(/ei/g,'ᛅᛁ') 
-	str = str.replace(/e/g,'ᛁ') 
-	str = str.replace(/é/g,'ᛁ') 
-	str = str.replace(/u/g,'ᚢ') 
-	str = str.replace(/ú/g,'ᚢ') 
-	str = str.replace(/í/g,'ᛁ') 
-	str = str.replace(/i/g,'ᛁ') 
-	str = str.replace(/o/g,'ᚢ')  
-	str = str.replace(/ó/g,'ᚢ')  
-	str = str.replace(/y/g,'ᚢ') 
-	str = str.replace(/ý/g,'ᚢ') 
-	str = str.replace(/æ/g,'ᛅ')
-	str = str.replace(/œ/g,'ᚢ')
-	str = str.replace(/ø/g,'ᚢ')
-	str = str.replace(/ǫ/g,'ᛅᚢ')
-	str = str.replace(/ã/g,'ᚨ')
-	
-	str = str.replace(/:/g,'᛬\u200B')
-	str = str.replace(/[,|.|-]/g,'')
-	str = str.replace(/͡/g,'\u200D')
-
-	// add markup for ambiguous cases
-	str = str.replace(/\[/g,'<span class=alts><span class=altfirst>')
-	str = str.replace(/\|/g,'</span><span class=alt>')
-	str = str.replace(/\{/g,'</span><span class=altlast>')
-	str = str.replace(/\]/g,'</span></span>')
-	str = str.replace(/¶/g,'')
-	
-	// remove duplicate runes in sequence
-	var runes = str.split('')
-	str = ''
-	for (var r=1;r<runes.length;r++) {
-		if (runes[r] != runes[r-1]) str += runes[r]
-		}
-
-
-	str = str.trim()
-	str = str.replace(/ /g,'᛬\u200B')
-
-	return str
-	}
-
-
-
-
-
-function oldNorseToShortRune (str) {
-	// transcribes vowelled Old Norse text into a rough ST runic transcription
-	
-	
-	
-	// add space so that beginning of word rules work
-	str = ' '+str.toLowerCase()+' '
-	str = str.replace(/[\s]+/g,' ')
-	
-	str = str.replace(/bb/g,'ᛓ') 
-	str = str.replace(/b/g,'ᛓ') 
-	str = str.replace(/dd/g,'ᛐ') 
-	str = str.replace(/d/g,'ᛐ') 
-	str = str.replace(/ð/g,'ᚦ') 
-	str = str.replace(/ff/g,'ᚠ') 
-	str = str.replace(/f/g,'ᚠ') 
-	str = str.replace(/gg/g,'ᚴ') 
-	str = str.replace(/g/g,'ᚴ') 
-	str = str.replace(/h/g,'ᚽ') 
-	str = str.replace(/j/g,'ᛁ') 
-	str = str.replace(/kk/g,'ᚴ') 
-	str = str.replace(/k/g,'ᚴ') 
+	str = str.replace(/bb/g,'ᛒ') 
+	str = str.replace(/b/g,'ᛒ') 
+	str = str.replace(/mm/g,'ᛗ') 
+	str = str.replace(/m/g,'ᛗ') 
 	str = str.replace(/ll/g,'ᛚ') 
 	str = str.replace(/l/g,'ᛚ') 
-	str = str.replace(/mm/g,'ᛙ') 
-	str = str.replace(/m/g,'ᛙ') 
-	str = str.replace(/nn/g,'ᚿ') 
-	str = str.replace(/n/g,'ᚿ') 
-	str = str.replace(/pp/g,'ᛓ') 
-	str = str.replace(/p/g,'ᛓ') 
-	str = str.replace(/rr/g,'ᚱ') 
-	str = str.replace(/r/g,'ᚱ') 
-	str = str.replace(/ʀ/g,'ᛧ') 
-	str = str.replace(/ss/g,'ᛌ') 
-	str = str.replace(/s/g,'ᛌ') 
-	str = str.replace(/tt/g,'ᛐ') 
-	str = str.replace(/t/g,'ᛐ') 
-	str = str.replace(/v/g,'ᚢ') 
-	str = str.replace(/þ/g,'ᚦ') 
-	str = str.replace(/v/g,'ᚢ') 
-	str = str.replace(/z/g,'ts') 
-	
-	str = str.replace(/a/g,'ᛆ') 
-	str = str.replace(/á/g,'ᛆ') 
-	str = str.replace(/ei/g,'ᛆᛁ') 
-	str = str.replace(/e/g,'ᛁ') 
-	str = str.replace(/é/g,'ᛁ') 
+	str = str.replace(/ŋ/g,'ᛝ') 
+	str = str.replace(/dd/g,'ᛞ') 
+	str = str.replace(/d/g,'ᛞ') 
+	str = str.replace(/ġ/g,'ᚸ') 
+	str = str.replace(/g̈/g,'ᚸ') 
+	str = str.replace(/kk/g,'ᛣ') 
+	str = str.replace(/k/g,'ᛣ') 
+	str = str.replace(/ċċ/g,'ᛤ') 
+	str = str.replace(/ċ/g,'ᛤ') 
+	str = str.replace(/k̈k̈/g,'ᛤ') 
+	str = str.replace(/k̈/g,'ᛤ') 
+
+	str = str.replace(/ea/g,'ᛠ') 
+	str = str.replace(/ēa/g,'ᛠ') 
+	str = str.replace(/a/g,'ᚪ') 
+	str = str.replace(/ā/g,'ᚪ') 
+	str = str.replace(/e/g,'ᛖ') 
+	str = str.replace(/ē/g,'ᛖ') 
 	str = str.replace(/u/g,'ᚢ') 
-	str = str.replace(/ú/g,'ᚢ') 
-	str = str.replace(/í/g,'ᛁ') 
+	str = str.replace(/ū/g,'ᚢ') 
 	str = str.replace(/i/g,'ᛁ') 
-	str = str.replace(/o/g,'ᚢ')  
-	str = str.replace(/ó/g,'ᚢ')  
-	str = str.replace(/y/g,'ᚢ') 
-	str = str.replace(/ý/g,'ᚢ') 
-	str = str.replace(/æ/g,'ᛆ')
-	str = str.replace(/œ/g,'ᚢ')
-	str = str.replace(/ø/g,'ᚢ')
-	str = str.replace(/ǫ/g,'ᛆᚢ')
-	str = str.replace(/ã/g,'ᚨ')
+	str = str.replace(/ī/g,'ᛁ') 
+	str = str.replace(/o/g,'ᚩ')  
+	str = str.replace(/ō/g,'ᚩ')  
+	str = str.replace(/y/g,'ᚣ') 
+	str = str.replace(/ȳ/g,'ᚣ') 
+	str = str.replace(/æ/g,'ᚫ')
+	str = str.replace(/ǣ/g,'ᚫ')
+	str = str.replace(/œ/g,'ᛟ')
+	str = str.replace(/ɨ/g,'ᛇ')
 	
+	str = str.replace(/:/g,'᛬\u200B')
+	str = str.replace(/[,|.|-]/g,'')
+	str = str.replace(/͡/g,'\u200D')
 
 	// add markup for ambiguous cases
 	str = str.replace(/\[/g,'<span class=alts><span class=altfirst>')
@@ -269,10 +186,6 @@ function oldNorseToShortRune (str) {
 	str = str.replace(/\]/g,'</span></span>')
 	str = str.replace(/¶/g,'')
 	
-	str = str.replace(/:/g,'᛬\u200B')
-	str = str.replace(/[,|.|-]/g,'')
-	str = str.replace(/͡/g,'\u200D')
-	
 	// remove duplicate runes in sequence
 	var runes = str.split('')
 	str = ''
@@ -280,50 +193,13 @@ function oldNorseToShortRune (str) {
 		if (runes[r] != runes[r-1]) str += runes[r]
 		}
 
+
 	str = str.trim()
 	str = str.replace(/ /g,'᛬\u200B')
 
 	return str
 	}
 
-
-
-
-
-function transcribeforGlossary (chstring) {
-	// creates a version of the string that will find results in Faulkes & Barnes glossary online			
-	chstring = chstring.replace(/Þ/g,'fi'); 
-	chstring = chstring.replace(/þ/g,'fl'); 
-	chstring = chstring.replace(/ð/g,'›'); 
-	chstring = chstring.replace(/ǫ/g,'ƒ'); 
-	chstring = chstring.replace(/Ǫ/g,'ā'); 
-	chstring = chstring.replace(/ý/g,'‡'); 
-	chstring = chstring.replace(/R/g,'ʀ'); 
-
-	return chstring;
-	}
-
-
-
-function transcribeFromNItON (chstring) {
-	// creates a Unicode version of a string copied from Faulkes & Barnes online documents			
-	chstring = chstring.replace(/fi/g,'Þ'); 
-	chstring = chstring.replace(/fl/g,'þ'); 
-	chstring = chstring.replace(/›/g,'ð'); 
-	chstring = chstring.replace(/ƒ/g,'ǫ'); 
-	chstring = chstring.replace(/ā/g,'Ǫ'); 
-	chstring = chstring.replace(/‡/g,'ý'); 
-	chstring = chstring.replace(/R/g,'ʀ'); 
-
-	return chstring;
-	}
-
-
-
-function openGlossaryWindow (str) {
-	var glossary = window.open('http://www.vsnrweb-publications.org.uk/Glossary%202011.pdf?s='+encodeURIComponent(str))
-	glossary.focus()
-	}
 
 
 
