ba6b31c76d7d5745fcf1e5ed31a417705e16da97
r12a <ishida@w3.org>
2016-02-08 22:05:55 +0000

    new egyptian hieroglyphs fonts.js

---
+++ scripts/egyptianhieroglyphs/fonts.js
@@ -0,0 +1,7 @@
+ var fontSelection = '<optgroup label="Google fonts ">\
+        <option value="Noto Sans Egyptian Hieroglyphs">Noto Sans Egyptian Hieroglyphs</option>\
+		</optgroup>\
+        \
+		<optgroup label="Other fonts ">\
+        <option value="Aegyptus">Aegyptus</option>\
+    	</optgroup>'
