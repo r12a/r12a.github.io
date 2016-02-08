d3f16ae55f21472279257623c38a3e7f4f8be22b
r12a <ishida@w3.org>
2016-02-08 20:29:32 +0000

    new egyptian hieroglyph picker, first draft only!

---
+++ pickers/egyptian/defaults.js
@@ -0,0 +1,14 @@
+var defaults = new Object;
+
+defaults.font = 'Doulos SIL'; // font name per usage in CSS
+defaults.size = "45";  // number of pixels (just the number)
+defaults.rows = "2"; // number representing number of times 100px for height of box
+defaults.lineHeight = "1.5";  // just the number
+defaults.view = "alphabet";  // alphabet, phonic, phonic2, phonelist
+defaults.language = 'am'
+defaults.blocklocation= '/scripts/latin/block'
+defaults.uifont = 'Doulos SIL'; // font name per usage in CSS
+defaults.uisize = "32";  // number of pixels (just the number)
+defaults.ccbase = "◌";  // default base for combining characters
+
+
