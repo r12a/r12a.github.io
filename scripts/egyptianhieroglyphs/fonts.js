c09664931c80c1dae81e004e121b78b367e3b26f
r12a <ishida@w3.org>
2016-02-24 20:58:19 +0000

    scripts/fonts, egyptian hieroglyphs, webfont added

--- scripts/egyptianhieroglyphs/fonts.js
+++ scripts/egyptianhieroglyphs/fonts.js
@@ -1,4 +1,8 @@
- var fontSelection = '<optgroup label="Google fonts ">\
+ var fontSelection = '<optgroup label="Webfonts ">\
+        <option value="Noto Sans Egyptian Hieroglyphs WF">Noto Sans Egyptian Hieroglyphs</option>\
+		</optgroup>\
+        \
+		<optgroup label="Google fonts ">\
         <option value="Noto Sans Egyptian Hieroglyphs">Noto Sans Egyptian Hieroglyphs</option>\
 		</optgroup>\
         \
