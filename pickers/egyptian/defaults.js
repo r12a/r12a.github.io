c37750b042b15d9c6ae831abe885ed60375291a2
r12a <ishida@w3.org>
2016-02-25 18:04:23 +0000

    line height 2 -> 3

--- pickers/egyptian/defaults.js
+++ pickers/egyptian/defaults.js
@@ -2,7 +2,7 @@ var defaults = new Object;
 
 defaults.font = 'Noto Sans Egyptian Hieroglyphs WF'; // font name per usage in CSS
 defaults.size = "45";  // number of pixels (just the number)
-defaults.rows = "2"; // number representing number of times 100px for height of box
+defaults.rows = "3"; // number representing number of times 100px for height of box
 defaults.lineHeight = "1.5";  // just the number
 defaults.view = "alphabet";  // alphabet, phonic, phonic2, phonelist
 defaults.language = 'egy'
