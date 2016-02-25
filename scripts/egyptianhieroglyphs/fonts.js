a3b70bdc0f7fad81cc1941256f37e2a1b9e53a9f
r12a <ishida@w3.org>
2016-02-25 10:18:11 +0000

    additional egyptian fonts added

--- scripts/egyptianhieroglyphs/fonts.js
+++ scripts/egyptianhieroglyphs/fonts.js
@@ -8,4 +8,7 @@
         \
 		<optgroup label="Other fonts ">\
         <option value="Aegyptus">Aegyptus</option>\
+        <option value="Gardiner">Gardiner</option>\
+        <option value="New Gardiner">New Gardiner</option>\
+        <option value="Nilus">Nilus</option>\
     	</optgroup>'
