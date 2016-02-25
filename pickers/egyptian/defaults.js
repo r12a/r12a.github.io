ff1d899e7c7079365b80468f65a34016b97fefbc
r12a <ishida@w3.org>
2016-02-25 09:44:58 +0000

    hieroglyphics -> heiroglyphs

--- pickers/egyptian/defaults.js
+++ pickers/egyptian/defaults.js
@@ -1,13 +1,13 @@
 var defaults = new Object;
 
-defaults.font = 'Noto Sans Egyptian Hieroglyphics WF'; // font name per usage in CSS
+defaults.font = 'Noto Sans Egyptian Hieroglyphs WF'; // font name per usage in CSS
 defaults.size = "45";  // number of pixels (just the number)
 defaults.rows = "2"; // number representing number of times 100px for height of box
 defaults.lineHeight = "1.5";  // just the number
 defaults.view = "alphabet";  // alphabet, phonic, phonic2, phonelist
 defaults.language = 'egy'
 defaults.blocklocation= '/scripts/egyptianhieroglyphs/block'
-defaults.uifont = 'Noto Sans Egyptian Hieroglyphics WF'; // font name per usage in CSS
+defaults.uifont = 'Noto Sans Egyptian Hieroglyphs WF'; // font name per usage in CSS
 defaults.uisize = "32";  // number of pixels (just the number)
 defaults.ccbase = "◌";  // default base for combining characters
 
