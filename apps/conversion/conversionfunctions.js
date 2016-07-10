6d23ed00c0366da487e2a275675c9748939d0467
r12a <ishida@w3.org>
2016-07-10 18:28:42 +0100

    apps/conversion, v8.1 adds rust escapes

--- apps/conversion/conversionfunctions.js
+++ apps/conversion/conversionfunctions.js
@@ -59,6 +59,9 @@ function displayResults(str, src) {
 	if (src != 'jEsc') { 
 		jEsc.value = convertCharStr2jEsc( str, getParameters(document.getElementById('jEscOptions')) );
 		}
+	if (src != 'rust') { 
+		rust.value = convertCharStr2Rust( str, getParameters(document.getElementById('rustOptions')) );
+		}
 	if (src != 'Unicode') { 
 		preserve = 'none';
 		if (document.getElementById('unicodelatin1').checked) { preserve = 'latin1'; }
@@ -335,6 +338,37 @@ function convertjEsc2Char ( str, shortEscapes ) {
 		str = str.replace(/\\\"/g, '\"'); 
 		str = str.replace(/\\\\/g, '\\'); 
 		}
+	return str;
+	}
+
+
+function convertRust2Char ( str ) { 
+	// converts a string containing Rust escapes to a string of characters
+	// str: string, the input
+	
+	// convert ES6-style escapes to characters
+	str = str.replace(/\\u\{([A-Fa-f0-9]{1,})\}/g, 
+					function(matchstr, parens) {
+						return hex2char(parens);
+						}
+						);
+	// convert \x and 2 digit escapes to characters
+	str = str.replace(/\\x([A-Fa-f0-9]{2})/g, 
+					function(matchstr, parens) {
+						return hex2char(parens);
+						}
+						);
+	// convert \b etc to characters
+		//str = str.replace(/\\0/g, '\0'); 
+		str = str.replace(/\\b/g, '\b'); 
+		str = str.replace(/\\t/g, '\t'); 
+		str = str.replace(/\\n/g, '\n'); 
+		str = str.replace(/\\v/g, '\v'); 
+		str = str.replace(/\\f/g, '\f'); 
+		str = str.replace(/\\r/g, '\r'); 
+		str = str.replace(/\\\'/g, '\''); 
+		str = str.replace(/\\\"/g, '\"'); 
+		str = str.replace(/\\\\/g, '\\'); 
 	return str;
 	}
 
@@ -895,7 +929,7 @@ function convertCharStr2jEsc ( str, parameters ) {
 					outputString += '\\U'+pad; 
 					}
 				else if (parameters.match(/es6styleSC/)) {
-					pad = suppCP.toString(16)
+					pad = suppCP.toString(16).toUpperCase()
 					outputString += '\\u{'+pad+'}' 
 					}
 				else {
@@ -927,11 +961,79 @@ function convertCharStr2jEsc ( str, parameters ) {
 				case 39: if (parameters.match(/noCR/)) {outputString += "\\\'";} else {outputString += '\''}; break;
 				case 92: outputString += '\\\\'; break;
 				default: 
-					if (cc > 0x1f && cc < 0x7F) { outputString += String.fromCharCode(cc); }
+					if (cc > 0x1f && cc < 0x7F) { 
+						outputString += String.fromCharCode(cc)
+						}
 					else { 
 						pad = cc.toString(16).toUpperCase();
 						while (pad.length < 4) { pad = '0'+pad; }
-						outputString += '\\u'+pad; 
+						outputString += '\\u'+pad 
+						}
+				}
+			}
+		}
+	return outputString;
+	}
+
+
+function convertCharStr2Rust ( str, parameters ) { 
+	// Converts a string of characters to Rust escapes
+	// str: sequence of Unicode characters
+	// parameters: a semicolon separated string showing ids for checkboxes that are turned on
+	var highsurrogate = 0;
+	var suppCP;
+	var pad;
+	var n = 0;
+	var pars = parameters.split(';')
+	var outputString = '';
+	for (var i = 0; i < str.length; i++) {
+		var cc = str.charCodeAt(i); 
+		if (cc < 0 || cc > 0xFFFF) {
+			outputString += '!Error in convertCharStr2UTF16: unexpected charCodeAt result, cc=' + cc + '!';
+			}
+		if (highsurrogate != 0) { // this is a supp char, and cc contains the low surrogate
+			if (0xDC00 <= cc && cc <= 0xDFFF) {
+				suppCP = 0x10000 + ((highsurrogate - 0xD800) << 10) + (cc - 0xDC00); 
+				pad = suppCP.toString(16).toUpperCase()
+				outputString += '\\u{'+pad+'}' 
+				highsurrogate = 0;
+				continue;
+				}
+			else {
+				outputString += 'Error in convertCharStr2UTF16: low surrogate expected, cc=' + cc + '!';
+				highsurrogate = 0;
+				}
+			}
+		if (0xD800 <= cc && cc <= 0xDBFF) { // start of supplementary character
+			highsurrogate = cc;
+			}
+		else { // this is a BMP character
+			//outputString += dec2hex(cc) + ' ';
+			switch (cc) {
+				case 0: outputString += '\\0'; break;
+				case 8: outputString += '\\b'; break;
+				case 9: if (parameters.match(/noCR/)) {outputString += '\\t';} else {outputString += '\t'}; break;
+				case 10: if (parameters.match(/noCR/)) {outputString += '\\n';} else {outputString += '\n'}; break;
+				case 13: if (parameters.match(/noCR/)) {outputString += '\\r';} else {outputString += '\r'}; break;
+				case 11: outputString += '\\v'; break;
+				case 12: outputString += '\\f'; break;
+				case 34: if (parameters.match(/noCR/)) {outputString += '\\\"';} else {outputString += '"'}; break;
+				case 39: if (parameters.match(/noCR/)) {outputString += "\\\'";} else {outputString += '\''}; break;
+				case 92: outputString += '\\\\'; break;
+				default: 
+					if (cc > 0x00 && cc < 0x20) { 
+						outputString += '\\x'+cc.toString(16).toUpperCase() 
+						}
+					else if (cc > 0x7E && cc < 0xA0) { 
+						outputString += '\\x'+cc.toString(16).toUpperCase() 
+						}
+					else if (cc > 0x1f && cc < 0x7F) { 
+						outputString += String.fromCharCode(cc)
+						}
+					else { 
+						pad = cc.toString(16).toUpperCase();
+						while (pad.length < 4) { pad = '0'+pad; }
+						outputString += '\\u{'+pad+'}'
 						}
 				}
 			}
