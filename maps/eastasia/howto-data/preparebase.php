370c9b87880216018d67cd862403215a4144e574
r12a <ishida@w3.org>
2016-09-06 09:39:45 +0100

    base now in svg_raw and svg, like other maps

--- maps/eastasia/howto-data/preparebase.php
+++ maps/eastasia/howto-data/preparebase.php
@@ -3,7 +3,7 @@
 # THIS IS FOR PREPARING THE FAREAST BASE MAP
 
 $nav = '';
-$filename = '../svg/base.svg';
+$filename = '../svg_raw/base.svg';
 		$fp = fopen($filename, "r");
 		if (!$fp) { $message = 'Could not open file.  Check the spelling of the URI.'; }
 	
@@ -23,9 +23,10 @@ $message = '';
 		$svgtext = preg_replace('/\<svg version="1\.1" xmlns="http\:\/\/www\.w3\.org\/2000\/svg" xmlns\:xlink="http:\/\/www\.w3\.org\/1999\/xlink" x="0px" y="0px"[\s]+viewBox="0 0 2905\.1 2313" style="enable-background\:new 0 0 2905\.1 2313;" xml\:space="preserve"\>/',' ',$svgtext);
 		$svgtext = preg_replace('/<text font-weight="700" font-family="Myriad Pro, Arial,(.|\n)+<\/svg>/','</svg>',$svgtext);
 		echo 'svgtext length '.strlen($svgtext)."<br/>";
-		$newsvgtext = str_replace("'MyriadPro-Regular'","'MyriadPro',Helvetica,Arial,sans-serif",$svgtext);
-		$newsvgtext = str_replace("'MyriadPro-Semibold'","'MyriadPro',Helvetica,Arial,sans-serif",$newsvgtext);
-		$newsvgtext = str_replace("'MyriadPro-Bold'","'MyriadPro',Helvetica,Arial,sans-serif",$newsvgtext);
+		$newsvgtext = str_replace("'MyriadPro-Regular'","'Myriad Pro',Helvetica,Arial,sans-serif",$svgtext);
+		$newsvgtext = str_replace("'MyriadPro-Semibold'","'Myriad Pro',Helvetica,Arial,sans-serif",$newsvgtext);
+		$newsvgtext = str_replace("'MyriadPro-Bold'","'Myriad Pro',Helvetica,Arial,sans-serif",$newsvgtext);
+		$newsvgtext = str_replace("'ArialMT'","'Myriad Pro',Helvetica,Arial,sans-serif",$newsvgtext);
 		$newsvgtext = str_replace('</svg>',$message,$newsvgtext)."\n";
 		
 		// change style class names
@@ -34,7 +35,7 @@ $message = '';
 	
 		echo 'updated svgtext length '.strlen($newsvgtext)."<br/>";
 		
-		if (is_writable('newbase.svg')) { echo "is writeable<br/>"; }
+		if (is_writable('../svg/base.svg')) { echo "is writeable<br/>"; }
 		else { echo "NOT WRITEABLE<br/>";  exit; }
 
 		//chmod($filename,0777);
@@ -46,8 +47,8 @@ $message = '';
 		//	exit;
 		//	}
 
-		$numbytes = file_put_contents('newbase.svg', $newsvgtext);
-  		echo "Wrote ($numbytes) bytes to file (newbase.svg)";
+		$numbytes = file_put_contents('../svg/base.svg', $newsvgtext);
+  		echo "Wrote ($numbytes) bytes to file (base.svg)";
 
 		//fclose($fp);
 
